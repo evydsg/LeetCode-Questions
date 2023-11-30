441a9924fbf2ad58fe0d2bb6351e995c9e89cbd9
Evelise Karen Guenda <118639499+evydsg@users.noreply.github.com>
2023-11-30 11:10:31 -0600

    concatenation

---
+++ concatenationArray.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans=[]
+        ans += nums * 2
+       
+        return ans
+        
